52adceedd292bbe0a1f0f845af155c897299232d
proyectoMMN <120412372+proyectoMMN@users.noreply.github.com>
2025-03-26 22:14:21 -0300

    Add files via upload

--- SintaxisMLM.R
+++ SintaxisMLM.R
@@ -15,15 +15,15 @@ library(ggplot2)
 
 ggplot(datosMLM, aes(x = Intencion_Conductual, 
                      y = Actividad_Fisica)) + 
-       geom_point(size = 1, 
-                  alpha = 0.5) +
-       geom_smooth(method = "lm",
-                   color = "grey20") +
-       facet_wrap(~IDvecindario) +
-       xlab("Intención conductual") + 
-       ylab("Actividad física semanal") +
-       ylim(0,7) +
-       theme_bw()
+  geom_point(size = 1, 
+             alpha = 0.5) +
+  geom_smooth(method = "lm",
+              color = "grey20") +
+  facet_wrap(~IDvecindario) +
+  xlab("Intención conductual") + 
+  ylab("Actividad física semanal") +
+  ylim(0,7) +
+  theme_bw()
 
 # Modelos de regresión multinivel ----
 
@@ -48,7 +48,6 @@ datosMLM$Intencion_ConductualCPG <- center(datosMLM$Intencion_Conductual,
                                            type = "CWC", 
                                            cluster = datosMLM$IDvecindario)
 
-
 ## 2) Modelo de intercepto aleatorio ----
 
 M.interaleatorio <- lmer(Actividad_Fisica ~ Intencion_ConductualCPG + Disp_Area +
@@ -60,6 +59,10 @@ summary(M.interaleatorio, ddf = "Kenward-Roger") # valores p método de Kenward-
 
 ## 3) Modelo de coeficientes aleatorios ----
 
+datosMLM$Actividad_Fisica <- as.numeric(datosMLM$Actividad_Fisica)
+datosMLM$Intencion_ConductualCPG <- as.numeric(datosMLM$Intencion_ConductualCPG)
+datosMLM$Disp_Area <- as.numeric(datosMLM$Disp_Area)
+
 M.coefaleatorios <- lmer(Actividad_Fisica ~ Intencion_ConductualCPG + Disp_Area + 
                            (1 + Intencion_ConductualCPG | IDvecindario), 
                          data = datosMLM, 
@@ -69,11 +72,27 @@ ranova(M.coefaleatorios)
 
 summary(M.coefaleatorios, ddf = "Kenward-Roger")
 
-## 4) Modelo de interceptos y pendientes como desenlace ----
+# Tamaño del efecto (R2) ----
 
-datosMLM$Actividad_Fisica <- as.numeric(datosMLM$Actividad_Fisica)
-datosMLM$Intencion_ConductualCPG <- as.numeric(datosMLM$Intencion_ConductualCPG)
-datosMLM$Disp_Area <- as.numeric(datosMLM$Disp_Area)
+library(r2mlm)
+
+r2 <- r2mlm(M.coefaleatorios)
+
+r2$Decompositions
+
+## Varianza intra-grupos explicada por el efecto fijo y aleatorio de intención
+0.2036128 + 0.1150949
+
+## Varianza total explicada por el efecto fijo y aleatorio de intención
+0.14987367 + 0.08471815
+
+## Varianza entre-grupos explicada por el efecto fijo de areas
+0.556718
+
+## Varianza total explicada por el efecto fijo de areas
+0.14693346
+
+## 4) Modelo de interceptos y pendientes como desenlace ----
 
 M.internivelcruzado <- lmer(Actividad_Fisica ~ Intencion_ConductualCPG + Disp_Area + 
                               Intencion_ConductualCPG:Disp_Area + 
@@ -83,26 +102,6 @@ M.internivelcruzado <- lmer(Actividad_Fisica ~ Intencion_ConductualCPG + Disp_Ar
 
 summary(M.internivelcruzado, ddf = "Kenward-Roger")
 
-# Tamaño del efecto (R2) ----
-
-library(r2mlm)
-
-r2 <- r2mlm(M.internivelcruzado)
-
-r2$Decompositions
-
-## Varianza intra-grupos explicada por el efecto fijo y aleatorio de intención
-0.26784401 + 0.06646512
-
-## Varianza total explicada por el efecto fijo y aleatorio de intención
-0.2038452 + 0.0505839
-
-## Varianza entre-grupos explicada por el efecto fijo de areas
-0.5107688
-
-## Varianza total explicada por el efecto fijo de areas
-0.1220435
 
 
 
-  
